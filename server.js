d6a4f8f3aaadcd1b0653895fcdfa441607a22f25
Ade1H <158167392+Ade1H@users.noreply.github.com>
2025-12-11 14:09:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const transporter = nodemailer.createTransport({
   secure: process.env.EMAIL_SECURE === "true",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS 
+    pass: process.env.EMAIL_PASS
   },
   tls: {
     rejectUnauthorized: false
